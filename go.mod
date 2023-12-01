260218424bedd0dc3d2cbbaa3f7b3cba31595e3e
Kyle Welch <1295580+kwelch@users.noreply.github.com>
2023-12-01 09:03:08 -1000

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kwelch/code-advent-2023
+
+go 1.21.3
---
+++ skeleton/dayX_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"testing"
+)
+
+var example = ``
+
+func Test_dayX_part1(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  int
+	}{
+		{
+			name:  "example",
+			input: example,
+			want:  -1,
+		},
+		{
+			name:  "actual",
+			input: input,
+			want:  -1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part1(tt.input); got != tt.want {
+				t.Errorf("part1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_dayX_part2(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  int
+	}{
+		{
+			name:  "example",
+			input: example,
+			want:  -1,
+		},
+		{
+			name:  "actual",
+			input: input,
+			want:  -1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := part2(tt.input); got != tt.want {
+				t.Errorf("part1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
